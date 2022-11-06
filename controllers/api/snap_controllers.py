556fe4f038a0facb42a1c6873565d3506444e025
Tega Ukavwe <justtega97@gmail.com>
2022-11-06 15:44:39 +0100

    added doc strings for snap_controllers

--- controllers/api/snap_controllers.py
+++ controllers/api/snap_controllers.py
@@ -5,6 +5,7 @@ from services.snaps_service import SnapService
 
 
 def health_check() -> None:
+    """Health check for the Snaps microservice."""
     return None
 
 
@@ -12,6 +13,13 @@ async def create_snap(
     snap: Snap,
     snap_service: SnapService = Depends(get_snap_service),
 ) -> DBSnap:
+    """
+    Creates a new Snapshot from the provided Snap.
+
+    :param snap: A dictionary mapping with the structure of the Pydantic Snap model.
+    :param snap_service: A SnapService instance to be injected for creation.
+    :return: A newly created snap.
+    """
     return await snap_service.create_snap(snap)
 
 
@@ -19,6 +27,13 @@ async def get_snap(
     snap_id: str,
     snap_service: SnapService = Depends(get_snap_service),
 ) -> DBSnap:
+    """
+    Retrieves a Snapshot from the database, raises a 404 if the Snap does not exist.
+
+    :param snap_id: id of the snap to be retrieved.
+    :param snap_service: A SnapService instance to be injected for snap retrieval.
+    :return: An existing snap, if the snap exists.
+    """
     snap = await snap_service.get_snap(snap_id)
     return snap.__dict__
 
@@ -26,5 +41,11 @@ async def get_snap(
 async def get_snaps(
     snap_service: SnapService = Depends(get_snap_service),
 ) -> list[DBSnap]:
+    """
+    Retrieves a Snapshot from the database, raises a 404 if the Snap does not exist.
+
+    :param snap_service: A SnapService instance to be injected for snaps retrieval.
+    :return: A list of existing snaps, if snaps exist.
+    """
     snaps = await snap_service.get_snaps()
     return [snap.__dict__ for snap in snaps]
