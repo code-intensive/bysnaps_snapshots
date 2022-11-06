65e4e0f0421f6a7f63025c83a854a2fa76f2ef8e
Tega Ukavwe <justtega97@gmail.com>
2022-11-06 15:46:11 +0100

    Handled exception raised when connection times out during snap upload to cloudinary

--- modules/cloud_snaps/cloudinary_snaps.py
+++ modules/cloud_snaps/cloudinary_snaps.py
@@ -1,13 +1,32 @@
 from cloudinary import CloudinaryImage, uploader
+from cloudinary.exceptions import Error as CloudinaryConnectionError
 from config.settings import settings
+from fastapi import HTTPException
+from fastapi.logger import logger
 from modules.cloud_snaps.interfaces.cloud_snap_interface import ICloudSnapService
+from starlette.status import HTTP_503_SERVICE_UNAVAILABLE
 
 
 class CloudinarySnapService(ICloudSnapService):
     @staticmethod
     async def upload_snap(snap: bytes) -> CloudinaryImage:
-        cloudinary_snap = uploader.upload_image(
-            file=snap,
-            folder=settings.CLOUDINARY_SNAP_UPLOAD_FOLDER,
-        )
-        return cloudinary_snap
+        try:
+            cloudinary_snap = uploader.upload_image(
+                file=snap,
+                folder=settings.CLOUDINARY_SNAP_UPLOAD_FOLDER,
+            )
+        except CloudinaryConnectionError as E:
+            logger.log(3, E.__str__(), exc_info=True)
+            detail = [
+                {
+                    "loc": [
+                        "cloudinary_snaps",
+                        "upload_snap",
+                    ],
+                    "type": "cloudinary.errors.connection_timeout",
+                    "msg": "Service temporarily unavailable, please try again",
+                },
+            ]
+            raise HTTPException(HTTP_503_SERVICE_UNAVAILABLE, detail=detail) from E
+        else:
+            return cloudinary_snap
