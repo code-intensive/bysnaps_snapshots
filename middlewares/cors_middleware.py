2a82fba0ea8c0be800ac1d59f6f818215d8bb810
Tega Ukavwe <justtega97@gmail.com>
2022-11-09 09:12:11 +0100

    ensure the snap_url column accomodates the cloudinary public_url

--- middlewares/cors_middleware.py
+++ middlewares/cors_middleware.py
@@ -1,11 +1,10 @@
+from config.settings import settings
 from fastapi.middleware.cors import CORSMiddleware
-
-origins = ("http://127.0.0.1:3000",)
 
 CORS_MIDDLEWARE_CONFIG = {
     "allow_methods": ["*"],
     "allow_headers": ["*"],
-    "allow_origins": origins,
+    "allow_origins": settings.ALLOWED_ORIGINS,
     "allow_credentials": True,
     "middleware_class": CORSMiddleware,
 }
